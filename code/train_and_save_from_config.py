11ae566ef422d9acea77f4609a4bcd1850519337
Ptisni <peter.tisnikar@gmail.com>
2024-06-18 22:37:37 +0100

    Edit the trainer from config

--- code/train_and_save_from_config.py
+++ code/train_and_save_from_config.py
@@ -3,7 +3,7 @@ import os
 import argparse
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument("--file", type=str, default="best_model_configs/best_minirocket_summerschool.json")
+argparser.add_argument("--file", type=str, default="best_minirocket_summerschool.json")
 args = argparser.parse_args()
 
 if os.getcwd().endswith("HRI-Error-Detection-STAI"):
@@ -16,7 +16,7 @@ print(args.file)
 trainer = TS_Model_Trainer(
     folder=pathprefix,
     n_jobs=4,
-    config_name=args.file
+    config_name="best_model_configs/" + args.file
 )
 
 trainer.train_and_save_best_model(
