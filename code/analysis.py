3e75597a50221cc0d7d8e1cfb5eaad9690b62894
Ptisni <peter.tisnikar@gmail.com>
2024-07-12 17:20:19 +0100

    Update MDI analysis code and plot

--- code/analysis.py
+++ code/analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import os
 import pickle
+import matplotlib.patches as mpatches
 
 
 class Analysis:
@@ -11,7 +12,7 @@ class Analysis:
         pass
 
     @staticmethod
-    def get_impurity_feature_importance(model: object, feature_names: list, plot: bool = True, save: bool = False) -> None:
+    def get_impurity_feature_importance(model: object, feature_names: list, plot: bool = True, save: bool = True, horizontal: bool = True) -> None:
         """
         method for interpretability of RFs
         """
@@ -25,25 +26,69 @@ class Analysis:
             [forest_importances, pd.Series(std, index=feature_names)], axis=1)
         forest_importances.columns = ["importance", "std"]
         forest_importances = forest_importances.sort_values(
-            by="importance", ascending=False)
-        print(forest_importances)
+            by="importance", ascending = False if horizontal else True)
 
         if plot:
-            fig, ax = plt.subplots()
+            fig = plt.figure(figsize=(20, 10)) if horizontal else plt.figure(figsize=(10, 18))
             # only plot the feature importances, and use std as error bars
-            forest_importances["importance"].plot.bar(
-                yerr=forest_importances["std"], ax=ax)
-            ax.set_title("Feature importances using MDI")
-            ax.set_ylabel("Mean decrease in impurity")
-            fig.tight_layout()
-            plt.show()
+            # change bar color depending on what dataset the feature belongs to: blue = openpose, orange = openface, green = opensmile, yellow = speaker diarization, red = frame
+            color = []
+            for feature in forest_importances.index:
+                if "openpose" in feature:
+                    color.append("blue")
+                elif "openface" in feature:
+                    color.append("orange")
+                elif "opensmile" in feature:
+                    color.append("green")
+                elif "speaker" in feature:
+                    color.append("purple")
+                elif "frame" in feature:
+                    color.append("red")
+                else:
+                    color.append("black")
+
+            # edit index to drop the _open* suffix
+            forest_importances.index = forest_importances.index.str.replace(
+                "_openpose", "")
+            forest_importances.index = forest_importances.index.str.replace(
+                "_openface", "")
+            forest_importances.index = forest_importances.index.str.replace(
+                "_opensmile", "")
+
+            blue_patch = mpatches.Patch(color='blue', label='Openpose')
+            orange_patch = mpatches.Patch(color='orange', label='Openface')
+            green_patch = mpatches.Patch(color='green', label='Opensmile')
+            purple_patch = mpatches.Patch(color='purple', label='Speaker diarization')
+            red_patch = mpatches.Patch(color='red', label='Frame')
+
+            if horizontal:
+                plt.grid(axis='both', linestyle='--', alpha=0.5)
+                plt.bar(forest_importances.index, forest_importances["importance"], yerr=forest_importances["std"], color=color)
+                plt.ylabel("Decrease in impurity")
+                plt.xticks(forest_importances.index, rotation=90)
+                plt.xlim([-1, len(forest_importances)])
+                plt.legend(handles=[blue_patch, orange_patch, green_patch, purple_patch, red_patch], prop={'size': 8})
+
+            else:
+                plt.grid(axis='both', linestyle='--', alpha=0.5)
+                plt.barh(forest_importances.index, forest_importances["importance"], xerr=forest_importances["std"], color=color)
+                plt.xlabel("Decrease in impurity")
+                plt.yticks(forest_importances.index)
+                plt.ylim([-1, len(forest_importances)])
+                plt.legend(handles=[blue_patch, orange_patch, green_patch, purple_patch, red_patch], loc='lower right', prop={'size': 20})
+            
+            # ensure the labels are readable
+            plt.tight_layout()
             if save:
-                fig.savefig("rf_feature_importances.pdf")
+                fig.savefig("plots/rf_feature_importances_horizonal.pdf" if horizontal else "plots/rf_feature_importances_vertical.pdf")
+            else:
+                plt.show()
 
 
 if __name__ == "__main__":
+
     if os.getcwd().endswith("HRI-Error-Detection-STAI"):
-        pathprefix = ""
+            pathprefix = ""
     else:
         pathprefix = "HRI-Error-Detection-STAI/"
 
@@ -55,4 +100,6 @@ if __name__ == "__main__":
     with open(pathprefix + "code/trained_models/" + model_to_load + "_columns.pkl", "rb") as f:
         features = pickle.load(f)
 
+
     r = Analysis.get_impurity_feature_importance(model, features)
+    r2 = Analysis.get_impurity_feature_importance(model, features, horizontal=False)
