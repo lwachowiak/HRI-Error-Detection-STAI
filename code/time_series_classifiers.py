f78e4d606e9418d6e8e0fa096e12925f03a378c3
Ptisni <peter.tisnikar@gmail.com>
2024-07-16 11:34:54 +0100

    Fix naive baseline

--- code/time_series_classifiers.py
+++ code/time_series_classifiers.py
@@ -906,7 +906,7 @@ class TS_Model_Trainer:
         columns_to_remove = self.column_removal_dict[columns_to_remove]
 
         val_X_TS_list, val_Y_TS_list, train_X_TS, train_Y_TS, column_order = self.data.get_summary_format(interval_length=interval_length, stride_train=stride_train,
-                                                                                                             stride_eval=stride_eval, fps=fps, verbose=True, label_creation=label_creation, task=task, rescaling=rescaling, fold=4)
+                                                                                                             stride_eval=stride_eval, fps=fps, label_creation=label_creation, task=task, rescaling=rescaling, fold=4)
         # nan handling based on training parameters
         if data_values["nan_handling"] == "zeros":
             val_X_TS_list = [np.nan_to_num(val_X_TS, nan=0)
