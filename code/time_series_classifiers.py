081aa725a2e673cd07b0d408f20b3abbf3cd6662
Ptisni <peter.tisnikar@gmail.com>
2024-07-16 11:41:20 +0100

    Another fix because I am a moron

--- code/time_series_classifiers.py
+++ code/time_series_classifiers.py
@@ -924,7 +924,7 @@ class TS_Model_Trainer:
             model, val_X_TS_list, interval_length=interval_length, stride_eval=stride_eval, model_type="Classic", start_padding=data_values["start_padding"])
         
         #replace val_preds with majority class prediction
-        val_preds = np.zeros(val_preds.shape).tolist()
+        val_preds = [0]*len(val_preds)
         val_scores = self.get_eval_metrics(
             val_preds, dataset="val", verbose=True)
         print("Val Scores:", val_scores)
