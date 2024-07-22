6e43b0addb8c3d72a601f19b6682db4d8e51d3bf
Ptisni <peter.tisnikar@gmail.com>
2024-07-22 14:35:10 +0100

    Switch back to grid search

--- code/time_series_classifiers.py
+++ code/time_series_classifiers.py
@@ -1055,10 +1055,10 @@ if __name__ == '__main__':
     # trainer.load_and_eval("RandomForest_2024-06-05-17")
 
     ########### uncomment to run optuna search ###########
-    # study_name = trainer.config["model_type"] + "_" + date
-    # gridsearch = "grid" in config_name
-    # study = trainer.optuna_study(
-    #    n_trials=trainer.config["n_trials"], model_type=trainer.config["model_type"], study_name=study_name, verbose=True, gridsearch=gridsearch)
+    study_name = trainer.config["model_type"] + "_" + date
+    gridsearch = "grid" in config_name
+    study = trainer.optuna_study(
+        n_trials=trainer.config["n_trials"], model_type=trainer.config["model_type"], study_name=study_name, verbose=True, gridsearch=gridsearch)
 
     # TODO: move to analysis script
     # feature importance
@@ -1071,5 +1071,5 @@ if __name__ == '__main__':
     #                       iterations_per_samplesize=4, stepsize=3, save_to=pathprefix+"plots/learning_curve_RF")
     #trainer.learning_curve("best_model_configs/ConvTranPlus_2024-07-13-14.json",
     #                       iterations_per_samplesize=4, stepsize=3, save_to=pathprefix+"plots/learning_curve_CT")
-    trainer.learning_curve("best_model_configs/TST_2024-07-16-10.json",
-                           iterations_per_samplesize=4, stepsize=3, save_to=pathprefix+"plots/learning_curve_TST")
+    #trainer.learning_curve("best_model_configs/TST_2024-07-16-10.json",
+    #                       iterations_per_samplesize=4, stepsize=3, save_to=pathprefix+"plots/learning_curve_TST")
